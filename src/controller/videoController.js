b738b6793402ae09df0b839b632ef815c8033bd6
jsw <allmie@naver.com>
2021-07-09 13:56:36 +0900

    feat: rendering vars
    - render시 vars 전달

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,6 +1,7 @@
-export const trending = (req, res) => res.render('home');
-export const search = (req, res) => res.send('Search');
-export const see = (req, res) => res.render('watch');
-export const edit = (req, res) => res.render('edit');
-export const deleteVideo = (req, res) => res.send('Delete Video');
-export const upload = (req, res) => res.send('Upload');
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
+export const search = (req, res) => res.send('search', { pageTitle: 'Search' });
+export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' });
+export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' });
+export const deleteVideo = (req, res) =>
+  res.send('delete Video', { pageTitle: 'Delete' });
+export const upload = (req, res) => res.send('upload', { pageTitle: 'Upload' });
